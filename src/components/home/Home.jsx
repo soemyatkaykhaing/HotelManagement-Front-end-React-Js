d93ca4172eaea00f89c0dde22959683b4a31abe8
kay khang <kaykhaing200@gmail.com>
2024-04-01 10:53:06 +0630

    frontend

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import Header from '../commom/Header'
-import MainHeader from '../layout/MainHeader'
-import HotelService from '../commom/HotelService'
-import Parallax from '../commom/Parallax'
-import RoomCarousel from '../commom/RoomCarousel'
-
+import React, { useContext } from "react"
+import MainHeader from "../layout/MainHeader"
+import HotelService from "../common/HotelService"
+import Parallax from "../common/Parallax"
+import RoomCarousel from "../common/RoomCarousel"
+import RoomSearch from "../common/RoomSearch"
+import { useLocation } from "react-router-dom"
+import { useAuth } from "../auth/AuthProvider"
 const Home = () => {
-  return (
-    <section>
-        <MainHeader/>
-        <div className='container'>
-          <RoomCarousel/>
-          <Parallax/>
-          <RoomCarousel/>
-          <HotelService/>
-          <Parallax/>
-          <RoomCarousel/>
-        </div>
-        
-    </section>
-  )
+	const location = useLocation()
+
+	const message = location.state && location.state.message
+	const currentUser = localStorage.getItem("userId")
+	return (
+		<section>
+			{message && <p className="text-warning px-5">{message}</p>}
+			{currentUser && (
+				<h6 className="text-success text-center"> You are logged-In as {currentUser}</h6>
+			)}
+			<MainHeader />
+			<div className="container">
+				<RoomSearch />
+				<RoomCarousel />
+				<Parallax />
+				<RoomCarousel />
+				<HotelService />
+				<Parallax />
+				<RoomCarousel />
+			</div>
+		</section>
+	)
 }
 
 export default Home
